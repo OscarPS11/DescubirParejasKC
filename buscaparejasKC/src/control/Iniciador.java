597c5c167d8539a61f6d60205f69665ca8471d03
OscarPS11 <oscarps11yt@gmail.com>
2018-03-22 09:21:24 +0100

    Crea tablero con casillas y le asigna un id

--- buscaparejasKC/src/control/Iniciador.java
+++ buscaparejasKC/src/control/Iniciador.java
@@ -1,5 +1,6 @@
 package control;
 
+import modelo.Casilla;
 import modelo.Tablero;
 
 public class Iniciador implements Iniciable{
@@ -7,15 +8,35 @@ public class Iniciador implements Iniciable{
 	@Override
 	public void iniciarJuego(Dificultad ParaFilaYColumna) {
 		// TODO Auto-generated method stub
-//		new Tablero(int fila, int columna),
+		tablero = new Tablero(ParaFilaYColumna.getValor(),ParaFilaYColumna.getValor());
+		rellenarTablero(tablero);
+		desordenarCasillas(tablero);
 //		RellenarTablero()crea una casilla por cada posicion del tablero y recibirá un número (referencia).
 //		DesordenarCasillas() el tablero queda con las casillas desordenadas.
+	}
+	
+	public void rellenarTablero(Tablero tablero){
+		Casilla casillas[][]=tablero.getCasilla();
+		for (int i = 0; i < casillas.length; i++) {
+			for (int j = 0; j < casillas[i].length; j++) {
+				casillas[i][j]=new Casilla(i);
+			}
+		}
 	}
 	public Tablero getTablero() {
 		return tablero;
 	}
 	public void setTablero(Tablero tablero) {
 		this.tablero = tablero;
+	}
+
+	public void desordenarCasillas(Tablero tablero) {
+		Casilla casillas[][]=tablero.getCasilla();
+		for (int i = 0; i < casillas.length; i++) {
+			for (int j = 0; j < casillas[i].length; j++) {
+				
+			}
+		}
 	}
 
 }
